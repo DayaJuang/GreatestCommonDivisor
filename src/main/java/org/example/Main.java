2814cc2a290a114cc6f6c4cc614f93551125dfee
daya <dayajuangmahaputra@gmail.com>
2022-11-09 20:18:17 +0900

    initial commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,29 @@
-package org.example;public class Main {
+package org.example;
+
+public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int ans = getGreatestCommonDivisor(15,20);
+        System.out.println(ans);
+    }
+
+    public static int getGreatestCommonDivisor(int a,int b){
+        // Check if the input is greater than 10
+        if(a < 10 || b < 10)
+            return -1;
+
+        // Find the largest divisor
+        int largestDivisor = 0;     // for keep updating the largest divisor
+        int divisorA = 1;
+        int divisorB = 1;
+
+        while(divisorA != a && divisorB != b){
+            if(a % divisorA == 0 && b % divisorB == 0){
+                if(divisorA > largestDivisor)
+                    largestDivisor = divisorA;
+            }
+            divisorA++;
+            divisorB++;
+        }
+        return largestDivisor;
     }
 }
